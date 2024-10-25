9564542e503b9a78a28485ccb339d9ce7c1f9034
apunn <aun_aunyamanee@hotmail.com>
2024-10-25 12:55:33 +0700

    fix: 🐛 position preview image

--- src/app/components/templates/ProductPreview.tsx
+++ src/app/components/templates/ProductPreview.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Text from "../atom/Text";
 import Script from "next/script";
 
-const ProductPreview:React.FC = () => {
+const ProductPreview: React.FC = () => {
   return (
-    <div className="inline-grid flex-col justify-center items-start w-full h-full pt-5 lg:pt-0 pb-16">
+    <div className="inline-grid grid-cols-1 justify-start items-start w-full h-full pt-5 lg:pt-0 pb-16">
       <div className="inline-grid grid-cols-2 gap-4 w-fit">
         <Text
           iconPrefix="https://d2cva83hdk3bwc.cloudfront.net/icon/r2s-display-medium.png"
@@ -20,12 +20,9 @@ const ProductPreview:React.FC = () => {
         src="/images/emis.jpeg"
         strategy="beforeInteractive" // Preload before the interactive part
       />
-     {  <Image
-        src="/images/emis.jpeg"
-        width={600}
-        height={600}
-        alt="product"
-      />}
+      <div className="w-full inline-grid justify-center">
+        <Image src="/images/emis.jpeg" width={600} height={600} alt="product" />
+      </div>
     </div>
   );
 };
